c80aed2293abbcfc0ac3e7a1d9f91b44bc0e3f30
James Gould <nobleburgundy@gmail.com>
2021-01-14 23:24:30 -0700

    Basic MVC setup

---
+++ controllers/club_controller.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const router = express.Router();
+const club = require("../models/club.js");
+const db = require("../models");
+
+// html routes TODO: separate to other files
+router.get("/", function (req, res) {
+  db.Club.findAll({}).then(function (data) {
+    // render the values of the data with spread operator
+    res.render("index", { clubs: [...data] });
+  });
+});
+
+// api routes - TODO: separate to other files
+router.get("/api/clubs", function (req, res) {
+  // get route for getting all clubs
+  const query = {};
+  db.Club.findAll({}).then(function (result) {
+    res.json(result);
+  });
+});
+
+router.post("/api/clubs", function (req, res) {
+  db.Club.create({ club_name: req.body.club_name }).then(function (result) {
+    res.json(result);
+  });
+});
+
+router.delete("/api/clubs/:id", function (req, res) {
+  const condition = { id: req.params.id };
+  club.delete(condition, function (result) {
+    if (result.affectedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+// Export the routes for server.js
+module.exports = router;

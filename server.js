0bd5ebaac6159ecf967a773ba0409e9a72f0839f
James Gould <nobleburgundy@gmail.com>
2021-01-16 12:55:30 -0700

    Updates to server.js to add new routes and passport session handling

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const db = require("./models");
+const session = require("express-session");
+// Requiring passport as we've configured it
+const passport = require("./config/passport");
 
 let PORT = process.env.PORT || 8080;
 const app = express();
@@ -19,6 +22,14 @@ app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
 const routes = require("./controllers/club_controller.js");
+// We need to use sessions to keep track of our user's login status
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Requiring our routes
+require("./controllers/html-controller.js")(app);
+require("./controllers/api-controller.js")(app);
 
 app.use(routes);
 

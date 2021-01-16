e8518be8215657772b21267a7fe7da0e19fecad4
James Gould <nobleburgundy@gmail.com>
2021-01-16 12:54:44 -0700

    Updated controllers to work with login/signup auth forms

--- controllers/html-controller.js
+++ controllers/html-controller.js
@@ -8,25 +8,26 @@ module.exports = function (app) {
   app.get("/", (req, res) => {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect("/members");
+      res.redirect("/index");
     }
-    // res.sendFile(path.join(__dirname, "../public/signup.html"));
-    res.render("signup");
+    res.render("login");
   });
 
   app.get("/login", (req, res) => {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect("/members");
+      res.redirect("/index");
     }
-    // res.sendFile(path.join(__dirname, "../public/login.html"));
     res.render("login");
+  });
+
+  app.get("/signup", (req, res) => {
+    res.render("signup");
   });
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/members", isAuthenticated, (req, res) => {
-    // res.sendFile(path.join(__dirname, "../public/members.html"));
-    res.render("members");
+  app.get("/index", isAuthenticated, (req, res) => {
+    res.render("index");
   });
 };

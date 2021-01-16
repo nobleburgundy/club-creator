4e7554487f92af6447d349e034f618399e9a6e34
phillipmerriman <phillipmerriman@gmail.com>
2021-01-16 13:12:51 -0600

    fix: updated length of location_zip

--- models/club.js
+++ models/club.js
@@ -32,7 +32,7 @@ module.exports = function (sequelize, DataTypes) {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        len: [1],
+        len: [5, 5],
       },
     },
     online_base_url: {

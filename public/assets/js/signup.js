f4c90a8350fc010df8e99d843c3b408f679df91d
James Gould <nobleburgundy@gmail.com>
2021-01-16 12:56:12 -0700

    Added public js files for jQuery login/signup forms

---
+++ public/assets/js/signup.js
@@ -0,0 +1,52 @@
+$(document).ready(() => {
+  // Getting references to our form and input
+  const signUpForm = $("form.signup");
+  const emailInput = $("input#email-input");
+  const passwordInput = $("input#password-input");
+  const firstNameInput = $("input#first-name-input");
+  const lastNameInput = $("input#last-name-input");
+  const userNameInput = $("input#username-input");
+
+  // When the signup button is clicked, we validate the email and password are not blank
+  signUpForm.on("submit", (event) => {
+    event.preventDefault();
+    const userData = {
+      firstName: firstNameInput.val().trim(),
+      lastName: lastNameInput.val().trim(),
+      userName: userNameInput.val().trim(),
+      email: emailInput.val().trim(),
+      password: passwordInput.val().trim(),
+    };
+
+    if (!userData.firstName || !userData.lastName || !userData.userName || !userData.email || !userData.password) {
+      return;
+    }
+    // If we have all the required userData, run the signUpUser function
+    console.log(userData);
+    signUpUser(userData.firstName, userData.lastName, userData.userName, userData.email, userData.password);
+    emailInput.val("");
+    passwordInput.val("");
+  });
+
+  // Does a post to the signup route. If successful, we are redirected to the members page
+  // Otherwise we log any errors
+  function signUpUser(firstName, lastName, userName, email, password) {
+    $.post("/api/signup", {
+      email: email,
+      password: password,
+      firstName: firstName,
+      lastName: lastName,
+      userName: userName,
+    })
+      .then(() => {
+        window.location.replace("/members");
+        // If there's an error, handle it by throwing up a bootstrap alert
+      })
+      .catch(handleLoginErr);
+  }
+
+  function handleLoginErr(err) {
+    $("#alert .msg").text(err.responseJSON);
+    $("#alert").fadeIn(500);
+  }
+});

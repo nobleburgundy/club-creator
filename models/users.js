d571fd045daec1f6aa4170c9f49811f30926f2a9
James Gould <nobleburgundy@gmail.com>
2021-01-18 20:21:47 -0700

    feat: new model for JoinedClubs table to track joined clubs by user

--- models/users.js
+++ models/users.js
@@ -38,7 +38,7 @@ module.exports = function (sequelize, DataTypes) {
         len: [1],
       },
     },
-  });
+});
 
   User.associate = function (models) {
     models.User.belongsTo(models.Club, {
@@ -48,6 +48,18 @@ module.exports = function (sequelize, DataTypes) {
       },
     });
   };
+
+  User.associate = function(models) {
+    models.User.belongsToMany(models.Club, {
+      through: "JoinedClubs",
+      foreignKey: {
+        name: "user_id",
+        allowNull: true
+      }
+    })
+  }
+
+
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
   User.prototype.validPassword = function (password) {
     return bcrypt.compareSync(password, this.password);

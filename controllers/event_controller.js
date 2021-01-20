ac3733d0830d61829c43b47840b33ffe14e3f4f9
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2021-01-19 19:23:12 -0600

    taking out extra space and club references in comment

--- controllers/event_controller.js
+++ controllers/event_controller.js
@@ -3,29 +3,28 @@ const router = express.Router();
 const event = require("../models/events.js");
 const db = require("../models");
 
-// index, club, user, event handlebars
+// index, event, user, event handlebars
 
 // show home page
 router.get("/", function (req, res) {
-  db.Event.findAll({}).then(function (data) {  // might need to change Club to reflect the right model
+  db.Event.findAll({}).then(function (data) {  // might need to change event to reflect the right model
     // render the values of the data with spread operator
     res.render("index", { event: [...data] });
   });
 });
 
-// show all events in api -- will need to change to show all clubs in a html page
+// show all events in api -- will need to change to show all events in a html page
 router.get("/api/events", function (req, res) {
-  // get route for getting all clubs
+  // get route for getting all events
   const query = {};
-  db.Event.findAll({}).then(function (result) { // might need to change Club to reflect the right model
+  db.Event.findAll({}).then(function (result) { // might need to change event to reflect the right model
     res.json(result);
   });
 });
 
-
-// html to show which club a user is in
+// html to show which event a user is in
 router.get("/api/users/:id", function (req, res) {
-  db.Event.findAll({ // might need to change Club to reflect the right model
+  db.Event.findAll({ // might need to change event to reflect the right model
     where: {
       users: req.body.users // probably need to change this reflect table/cells name
     }
@@ -36,7 +35,7 @@ router.get("/api/users/:id", function (req, res) {
 
 // View single event
 router.get("/api/event/:id", function (req, res) {
-  db.Event.findAll({ // might need to change Club to reflect the right model
+  db.Event.findAll({ // might need to change event to reflect the right model
     where: {
       event_name: req.body.event_name // probably need to change thisreflect table/cells name
     }
@@ -45,10 +44,9 @@ router.get("/api/event/:id", function (req, res) {
   });
 });
 
-
 // Create a new event
 router.post("/api/events", function (req, res) {
-  // might need to change Club to reflect the right model
+  // might need to change event to reflect the right model
   db.Event.create({ event_name: req.body.event_name }).then(function (result) {
     res.json(result);
   });

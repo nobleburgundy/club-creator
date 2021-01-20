82a8b68af31eb1dd71e08369b5a926302201ff9d
James Gould <nobleburgundy@gmail.com>
2021-01-20 13:53:01 -0700

    feat: Updated index html route to account for authentication

--- controllers/html-controller.js
+++ controllers/html-controller.js
@@ -28,12 +28,17 @@ module.exports = function (app) {
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/index", isAuthenticated, (req, res) => {
+  app.get("/index", (req, res) => {
     // This will render only the clubs the user belongs to on the home "index" page (also / since redirect is in-place in html-controller)
     // req.user holds the user information after login
-    db.Club.findAll({
-      include: { model: db.User, as: "Users", where: { id: req.user.id } },
-    }).then(function (data) {
+    let query = "";
+    if (req.user) {
+      query = { include: { model: db.User, as: "Users", where: { id: req.user.id } } };
+    } else {
+      // if not logged in, show all clubs
+      query = {};
+    }
+    db.Club.findAll(query).then(function (data) {
       res.render("index", { clubs: [...data] });
     });
   });

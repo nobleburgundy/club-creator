58c4995a727421a038d0cd260c0a951adab9046a
Phillip Merriman <phillipmerriman@gmail.com>
2021-01-20 14:48:26 -0600

    fix: updated src on' card img

--- controllers/html-controller.js
+++ controllers/html-controller.js
@@ -32,6 +32,7 @@ module.exports = function (app) {
       res.render("clubs", { clubs: [...data] });
     });
   });
+  
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page

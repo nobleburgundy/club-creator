3bfd4be0e7422d86210a729215be463fc7baa267
James Gould <nobleburgundy@gmail.com>
2021-01-21 19:46:53 -0700

    feat: added jQuery for handling search bar.

--- public/assets/js/orbit.js
+++ public/assets/js/orbit.js
@@ -1,1 +1,14 @@
 // Front-End jQuery Code
+$(document).ready(() => {
+  const searchForm = $("form#search-form");
+  const searchInput = $("input#stretch");
+  const searchButton = $("button#search-button");
+
+  searchForm.on("submit", (event) => {
+    event.preventDefault();
+    const query = searchInput.val();
+    // handlebars /search route will handle rendering the data, so just nav to
+    // /search with the query parameter
+    window.location.replace("/search?q=" + query);
+  });
+});

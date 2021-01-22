df0a2e013e633509cb037f4dc70a882fec9639e3
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2021-01-21 19:13:07 -0600

    Creating event listenner for create event and club

--- public/assets/js/orbit.js
+++ public/assets/js/orbit.js
@@ -1,1 +1,35 @@
 // Front-End jQuery Code
+
+// Event listenner for create event
+$(".create_event").on("submit", function (event) {
+  event.preventDefault();
+
+  let newEvent = {
+    event_name: $("#event_name").val().trim(),
+  };
+  $.ajax("/api/event", {
+    type: "POST",
+    data: newEvent,
+  }).then(function () {
+    console.log("New event created");
+
+    location.reload();
+  });
+});
+
+// Event listenner for create club
+$(".create_club").on("submit", function (event) {
+  event.preventDefault();
+
+  let newEvent = {
+    event_name: $("#club_name").val().trim(),
+  };
+  $.ajax("/api/clubs", {
+    type: "POST",
+    data: newEvent,
+  }).then(function () {
+    console.log("New club created");
+
+    location.reload();
+  });
+});

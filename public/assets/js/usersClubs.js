cba42d4845c28d33dde6f693b9f17fec3375d42c
James Gould <nobleburgundy@gmail.com>
2021-01-24 08:58:04 -0700

    Removed commented out code.

--- public/assets/js/usersClubs.js
+++ public/assets/js/usersClubs.js
@@ -1,21 +1,11 @@
 // this is a jquery file to check if a user has joined a specific club and update the DOM accordingly
 $(document).ready(() => {
-  // api call to /users/
-  //   $.get("/api/user_data").then((data) => {
-  //     console.log("data = " + data);
-  //     // get joined clubs /api/clubs/user/:id
-  //     $.get("/api/clubs/user/" + data.id).then((clubs) => {
-  //       // gets joined clubs by id
-  //       const ids = clubs.map((e) => e.id);
-  //     });
-  //   });
-
   function getCurrentUserClubs(cb) {
+    // api call to /user_data/ to get id
     $.get("/api/user_data").then((data) => {
-      console.log("data = " + data);
       // get joined clubs /api/clubs/user/:id
       $.get("/api/clubs/user/" + data.id).then((clubs) => {
-        // gets joined clubs by id
+        // gets joined clubs by id and return array of ids
         const ids = clubs.map((e) => e.id);
         return cb(ids);
       });
